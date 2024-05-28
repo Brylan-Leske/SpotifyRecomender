7cafc8cbd2af82563c6660ab93a629e4b12dfcfb
Brylan-Leske <55639217+Brylan-Leske@users.noreply.github.com>
2024-05-28 00:47:53 -0400

    Update script.js
    fixed redirect url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const clientId = 'ae92d6a5af594310b5a621552b4410ce'; // your clientId
-const redirectUrl = 'http://localhost:8080';        // your redirect URL - must be localhost URL and/or HTTPS
+const redirectUrl = 'https://brylan-leske.github.io/SpotifyRecomender/';        // your redirect URL - must be localhost URL and/or HTTPS
 
 const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 const tokenEndpoint = "https://accounts.spotify.com/api/token";
